11f4338363a1ea960d36eb968f9241dd804e9e84
omastrangelo <octamastrangelo@gmail.com>
2023-04-12 19:51:29 -0300

    Primera entrega Proyecto

---
+++ src/app.js
@@ -0,0 +1,23 @@
+//Imports.
+import express from "express";
+import productsRouter from "./routes/products.router.js"
+import cartsRouter from "./routes/carts.router.js";
+import __dirname from "./utils.js";
+
+//Create express app and their ports.
+const app = express();
+const PORT = 8080;
+
+//Listen '8080' ports.
+app.listen(PORT, ()=>{
+    console.log(`Server started on ${PORT} ports.`)
+})
+
+//static(public directory), urlencoded & json.
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'))
+
+//Routes 'products' & 'carts'.
+app.use('/api/products',productsRouter)
+app.use('/api/carts', cartsRouter)
